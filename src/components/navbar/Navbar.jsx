a9c975024e6cce05d9110e098b8732f6f0cf96da
Min Thu Khaing <mtk.min2003@gmail.com>
2024-10-27 23:36:30 +0630

    that is right navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import { motion } from "framer-motion";
-
 import LaptopNav from "./LaptopNav";
 import MobileNav from "./MobileNav";
 import { UpdateFollower } from "react-mouse-follower";
@@ -9,19 +8,25 @@ import { UpdateFollower } from "react-mouse-follower";
 const Navbar = ({ className }) => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [navbarClass, setNavbarClass] = useState("");
+  const [timeoutId, setTimeoutId] = useState(null);
 
   const HandleScroll = () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
     if (window.scrollY > 0) {
       setIsScrolled(true);
-      setNavbarClass(
-        "fixed inset-0 h-[80px]  bg-slate-400 bg-opacity-55 z-[999999]"
-      );
-      setTimeout(() => {
+      setNavbarClass("fixed inset-0 h-[80px] bg-slate-400 bg-opacity-85 z-[999999]");
+      
+      const id = setTimeout(() => {
         setNavbarClass("hidden duration-500 delay-300 ease");
-        return;
-      },9000);
+      }, 2000);
+      
+      setTimeoutId(id);
     } else {
       setIsScrolled(false);
+      setNavbarClass("");
     }
   };
 
@@ -30,7 +35,7 @@ const Navbar = ({ className }) => {
     return () => {
       window.removeEventListener("scroll", HandleScroll);
     };
-  }, []);
+  }, [timeoutId]);
 
   return (
     <motion.div
@@ -39,9 +44,7 @@ const Navbar = ({ className }) => {
         opacity: 1,
         transition: { duration: 0.5, delay: 0.4, ease: "easeInOut" },
       }}
-      className={`container ${
-        isScrolled && `${navbarClass}  `
-      } text-white py-3 flex items-center justify-between`}
+      className={`container ${navbarClass} text-white py-3 flex items-center justify-between`}
     >
       {/* logo section */}
       <div>
