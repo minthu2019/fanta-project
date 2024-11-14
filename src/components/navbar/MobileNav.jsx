b3863fdc3c492b1a2f168e4e9dad9cce3bfa5cd5
Min Thu Khaing <mtk.min2003@gmail.com>
2024-11-14 23:17:41 +0630

    hello

--- src/components/navbar/MobileNav.jsx
+++ src/components/navbar/MobileNav.jsx
@@ -41,7 +41,7 @@ const MobileNav = () => {
           <Menu size={30} color="white" />
         </SheetTrigger>
 
-        <SheetContent className="h-screen flex items-center justify-center flex-col invert z-40">
+        <SheetContent className="h-screen flex items-center justify-center flex-col invert z-50">
           <div className="flex items-center justify-center pb-10 ">
             <img src={logo} alt="logo" className="w-36" />
           </div>
